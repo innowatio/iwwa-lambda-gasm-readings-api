a9b33e5862877605b53f216115f149592ecd2753
clevergy1 <marco.fagnano@clevergy.it>
2016-02-10 17:04:19 +0100

    some style and momente dependencies

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME || 'KINESIS_STREAM_NAME';
+export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME || "KINESIS_STREAM_NAME";
 export const KINESIS_PARTITION_KEY = "iwwa-lambda-gasm-readings-api";
 export const NODE_ENV = process.env.NODE_ENV;
--- src/index.js
+++ src/index.js
@@ -4,164 +4,158 @@ import connect from "lambda-connect";
 import log from "./services/logger";
 import * as config from "./config";
 import * as bitConverter from  "./services/BinaryParser";
-var moment = require('moment');
+var moment = require("moment");
 
-    const kinesis = new Kinesis();
+const kinesis = new Kinesis();
 
-    const dispatch = getDispatch({
-        kinesisClient: kinesis,
-        kinesisStream: config.KINESIS_STREAM_NAME,
-        producerId: "server@hostname"
-    });
+const dispatch = getDispatch({
+    kinesisClient: kinesis,
+    kinesisStream: config.KINESIS_STREAM_NAME,
+    producerId: "server@hostname"
+});
 
 export const handler = connect({log})   
-    .use(function (req){
+    .use(function (req) {
   
-    var myBinaryParser = (new bitConverter.BinaryParser(true, true));
-    var m_Model ={}; 
-    for (var prop in req.params) {
-        var thenum = prop.replace(/^\D+/g, '');                                
-        if (thenum !='') {
-            var block = prop.replace(thenum, '');
-            
-            if (block == 'GASM') {
-                m_Model.sensorId=req.params.ID+'_'+prop;
-                m_Model.date=parse_dtLog(req.params.D);                
+        var myBinaryParser = (new bitConverter.BinaryParser(true, true));
+        var m_Model ={}; 
+        for (var prop in req.params) {
+            var thenum = prop.replace(/^\D+/g, "");                                
+            if (thenum !="") {
+                var block = prop.replace(thenum, "");
                 
-                var measurementsList=[];
-                var  measurements={};
-                var data =req.params[prop].split(';'); // req.params.GASM01.split(';');
-                if (data.length >= 0) { 
-                    measurements={}
-                    measurements.type='CorrectedGasCounter';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                    
-                    measurements.value = myBinaryParser.toDouble(hex2bin(data[0]));
-                    measurementsList.push(measurements);
-                }
-                if (data.length >= 1) {
-                    measurements={};
-                    measurements.type='MeasuredGasCounter';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                     
-                    measurements.value=data[1];
-                    measurementsList.push(measurements);
-                 }  
-                if (data.length >= 2) {
-                    measurements={};
-                    measurements.type='Pressure';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='bar';                      
-                    measurements.value=data[2];
-                    measurementsList.push(measurements);
-                 } 
-                if (data.length >= 3) {
-                    measurements={};
-                    measurements.type='Temperature';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='°C';                      
-                    measurements.value=data[3];
-                    measurementsList.push(measurements);
-                 }  
-                if (data.length >= 4) {
-                    measurements={};
-                    measurements.type='ConversionCostant';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='';                      
-                    measurements.value=data[4];
-                    measurementsList.push(measurements);
-                 } 
-                if (data.length >= 5) {
-                    measurements={};
-                    measurements.type='CorrectedVolInErrCond';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                      
-                    measurements.value=myBinaryParser.toDouble(hex2bin(data[5]));
-                    measurementsList.push(measurements);
-                 }  
-                if (data.length >= 6) {
-                    measurements={};
-                    measurements.type='CorrectedVolDay';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                      
-                    measurements.value=myBinaryParser.toDouble(hex2bin(data[6]));
-                    measurementsList.push(measurements);
-                 } 
-                if (data.length >= 7) {
-                    measurements={};
-                    measurements.type='CorrectedVolMonth';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                      
-                    measurements.value=myBinaryParser.toDouble(hex2bin(data[7]));
-                    measurementsList.push(measurements);
-                 } 
-                if (data.length >= 8) {
-                    measurements={};
-                    measurements.type='MeasuredVolDay';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3';                      
-                    measurements.value=data[8];
-                    measurementsList.push(measurements);
-                 }  
-                if (data.length >= 9) {
-                    measurements={};
-                    measurements.type='MeasuredFlow';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3/h';                      
-                    measurements.value=data[9];
-                    measurementsList.push(measurements);
-                 }
-                if (data.length >= 10) {
-                    measurements={};
-                    measurements.type='CorrectedFlow';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='m3/h';                      
-                    measurements.value=data[10];
-                    measurementsList.push(measurements);
-                 }  
-                if (data.length >= 11) {
-                    measurements={};
-                    measurements.type='BatteryCapacity';
-                    measurements.source='reading';  
-                    measurements.unitOfMeasurement='%';                      
-                    measurements.value=data[11]/10;
-                    measurementsList.push(measurements);
-                 }                                                                                                                                                      
-                 
-                  
-                 m_Model.measurements=measurementsList;                
-            }                                    
-        } 
-    }//end for
+                if (block == "GASM") {
+                    m_Model.sensorId=req.params.ID+"_"+prop;
+                    m_Model.date=parse_dtLog(req.params.D);                
+                    
+                    var measurementsList=[];
+                    var  measurements={};
+                    var data =req.params[prop].split(";"); 
+                    if (data.length >= 0) { 
+                        measurements={};
+                        measurements.type="CorrectedGasCounter";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                    
+                        measurements.value = myBinaryParser.toDouble(hex2bin(data[0]));
+                        measurementsList.push(measurements);
+                    }
+                    if (data.length >= 1) {
+                        measurements={};
+                        measurements.type="MeasuredGasCounter";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                     
+                        measurements.value=data[1];
+                        measurementsList.push(measurements);
+                    }  
+                    if (data.length >= 2) {
+                        measurements={};
+                        measurements.type="Pressure";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="bar";                      
+                        measurements.value=data[2];
+                        measurementsList.push(measurements);
+                    } 
+                    if (data.length >= 3) {
+                        measurements={};
+                        measurements.type="Temperature";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="°C";                      
+                        measurements.value=data[3];
+                        measurementsList.push(measurements);
+                    }  
+                    if (data.length >= 4) {
+                        measurements={};
+                        measurements.type="ConversionCostant";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="";                      
+                        measurements.value=data[4];
+                        measurementsList.push(measurements);
+                    } 
+                    if (data.length >= 5) {
+                        measurements={};
+                        measurements.type="CorrectedVolInErrCond";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                      
+                        measurements.value=myBinaryParser.toDouble(hex2bin(data[5]));
+                        measurementsList.push(measurements);
+                    }  
+                    if (data.length >= 6) {
+                        measurements={};
+                        measurements.type="CorrectedVolDay";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                      
+                        measurements.value=myBinaryParser.toDouble(hex2bin(data[6]));
+                        measurementsList.push(measurements);
+                    } 
+                    if (data.length >= 7) {
+                        measurements={};
+                        measurements.type="CorrectedVolMonth";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                      
+                        measurements.value=myBinaryParser.toDouble(hex2bin(data[7]));
+                        measurementsList.push(measurements);
+                    } 
+                    if (data.length >= 8) {
+                        measurements={};
+                        measurements.type="MeasuredVolDay";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3";                      
+                        measurements.value=data[8];
+                        measurementsList.push(measurements);
+                    }  
+                    if (data.length >= 9) {
+                        measurements={};
+                        measurements.type="MeasuredFlow";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3/h";                      
+                        measurements.value=data[9];
+                        measurementsList.push(measurements);
+                    }
+                    if (data.length >= 10) {
+                        measurements={};
+                        measurements.type="CorrectedFlow";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="m3/h";                      
+                        measurements.value=data[10];
+                        measurementsList.push(measurements);
+                    }  
+                    if (data.length >= 11) {
+                        measurements={};
+                        measurements.type="BatteryCapacity";
+                        measurements.source="reading";  
+                        measurements.unitOfMeasurement="%";                      
+                        measurements.value=data[11]/10;
+                        measurementsList.push(measurements);
+                    } 
+                    
+                    m_Model.measurements=measurementsList;                
+                }                                    
+            } 
+        } // end for
 
+        const eventType = "element inserted in collection gasm-raw-reading";
+        const eventData = m_Model;
+        const eventOptions = {
+            sourceUserId: "",
+            partitionKey: config.KINESIS_PARTITION_KEY
+        };
+        return dispatch(eventType, eventData, eventOptions);
 
-    //return m_Model; 
-    
-
-
-    const eventType = "element inserted in collection gasm-raw-reading";
-    const eventData = m_Model;
-    const eventOptions = {
-        sourceUserId: "",
-        partitionKey: config.KINESIS_PARTITION_KEY
-    };
-    return dispatch(eventType, eventData, eventOptions);
-
-});
+    });
 
 /*
 * date parser
 ---------------------------------------------- */
-function parse_dtLog(d){
+function parse_dtLog (d) {
     return moment(d, "YYYY-MM-DD-HH:mm:ss").toISOString();
 }
 
 /*
- * hex2bin 
- * -----------------------------------------------------------------------*/
-function hex2bin(s) {
+* hex2bin 
+* -----------------------------------------------------------------------*/
+function hex2bin (s) {
     for (var i = 0, l = s.length, r = ""; i < l; r += String.fromCharCode(parseInt(s.substr(i, 2), 16)), i += 2)    ;
     return r;
 }
-/*------------------------------------------------------------------------*/
+/* ------------------------------------------------------------------------*/
 
