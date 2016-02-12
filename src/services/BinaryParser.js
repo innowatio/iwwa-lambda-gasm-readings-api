c3b00474dddf085cbb587fc2896d6dbfb68631f6
Clevergy account <clevergy.app@outlook.com>
2016-02-12 13:19:51 +0100

    one more lint correction

--- src/services/BinaryParser.js
+++ src/services/BinaryParser.js
@@ -4,7 +4,7 @@
  
 export function toDouble (m_hex) {
     var buf = new Buffer(8);
-    var bufIdx=0 
+    var bufIdx=0;
     for (var i = 0; i < m_hex.length; i+=2) {
         buf[bufIdx] = "0x" + m_hex.substr(i, 2);
         bufIdx++;
--- test/index.js
+++ test/index.js
@@ -12,12 +12,10 @@ chai.use(sinonChai);
 
 
 describe("gasm readings handler", () => {
-
     var dispatchSpy = sinon.stub().returns(Promise.resolve());
     indexModule.__Rewire__("dispatch", dispatchSpy);        
 
     it("calls dispatch function with the correct parameters", (done) => {
-        // var fakeRequest ={"path":"/?ID=5&V=1&D=2016-02-04-16:03:50&S=0&GASM01=41033AC25BAC7000;161289;1.061227;17.43649;1.038604;40C3C68CD84A0000;407B3A16C7400000;40AF258AE3200000;414;64.73656;67.23566;996;0&CTB01=101;97;0;0;72525;0&CTB02=108;96;105550;142703;73918;0&CTB03=96;98;0;0;73143;0&CTB04=686;675;86400;108967;102066;0"};
         var fakeRequest = {
             "url":"/", 
             "method":"GET", 
@@ -36,13 +34,13 @@ describe("gasm readings handler", () => {
             }
         };
         var fakeContext ={"succeed": sinon.spy(), "fail": sinon.spy()};
-        var result = handler(fakeRequest, fakeContext);         
+        var promise = handler(fakeRequest, fakeContext);
+               
         // assertions
-        result.then(function (result) {
+        promise.then(function () {
             expect(dispatchSpy).to.have.callCount(1);
-            expect(dispatchSpy).to.have.been.calledWith("element inserted in collection gasm-raw-reading");
-            
-            var secondParameter = dispatchSpy.getCall(0).args[1];
+            expect(dispatchSpy).to.have.been.calledWith("element inserted in collection gasm-raw-reading");            
+            var secondParameter =  dispatchSpy.getCall(0).args[1];           
             expect(secondParameter).to.deep.equal(expectedOutpu1);
             done();
         }, function (error) {
